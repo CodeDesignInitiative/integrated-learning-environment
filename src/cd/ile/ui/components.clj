9a07790ee042846ee870617181601b0fee66884c
paulHempel <github@paul-hempel.de>
2023-02-26 15:22:32 +0100

    fancy chat page

--- src/cd/ile/ui/components.clj
+++ src/cd/ile/ui/components.clj
@@ -8,7 +8,7 @@
     {:href     href
      :disabled (or disabled? false)}
     [:div.p-2.rounded-xl.hover:shadow-lg.transition-all.aspect-square
-     {:class    bg-color}
+     {:class bg-color}
      [:img.p-4.invert {:src (str "/img/icons/" (or icon "missing") ".svg")}]]
 
     [:div.text-center.mt-2 label]]
@@ -22,3 +22,35 @@
     (map
       (fn [n] [:li.leading-loose n])
       notes)]])
+
+(defn video-widget
+  "Displays a YouTube video embed, if video URL is not nil."
+  [video]
+  (when video
+    [:iframe.rounded-xl.mb-2
+     {:width           "560"
+      :height          "315"
+      :src             video
+      :title           "YouTube video player"
+      :frameborder     "0"
+      :allow           "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      :allowfullscreen "true"}]))
+
+(defn conversation-widget
+  "Displays the messages of a given conversation without providing any interaction."
+  [conversation]
+  (map
+    (fn [prev-conv]
+      [:div.mb-10.m-3.max-w-2xl.mx-auto.px-3
+       (map (fn [{:story/keys [person message video answer-choices]}]
+              [:<>
+               [:div.py-3.px-4.rounded-xl.bg-green-400.mb-2.mr-5
+                [:h3.text-sm (:person/name person)]
+                (video-widget video)
+                [:p.m-0 message]]
+               (when answer-choices
+                 [:div.py-3.px-4.rounded-xl.bg-sky-400.mb-2.ml-5
+                  [:h3.text-sm "Du"]
+                  [:p.m-0 (first answer-choices)]])])
+            prev-conv)])
+    conversation))
--- src/cd/ile/views/chat_screen.clj
+++ src/cd/ile/views/chat_screen.clj
@@ -1,24 +1,40 @@
-(ns cd.ile.views.chat-screen)
+(ns cd.ile.views.chat-screen
+  (:require [cd.ile.ui.components :as components]))
 
 (defn chat-screen []
   (let [conversations cd.ile.core.mock-data/conversations]
     [:div.bg-gradient-to-b.min-h-screen.text-white
-     [:h1.text-xl.p-3 "Chats"]
-     [:div.flex.flex-row.gap-2
-      [:div.border-r.border-slate-300.border-solid
+     [:h1.text-xl.p-3.text-center.border-b.border-zinc-500.border-solid.bg-zinc-800
+      "Chats"]
+     [:div.flex.flex-row
+      [:div.bg-zinc-700.w-48
 
        (map (fn [c]
-              [:div.p-3.border-t.border-slate-300.border-solid
+              [:div.p-3.border-b.border-zinc-600.border-solid
                (get-in c [:conversation/with :person/name])
-               ])
+               [:p.m-0.text-sm
+                (get-in c [:conversation/with :person/organization])]])
             conversations)]
-      [:div
-       (map
-         (fn [prev-conv]
-           [:div.mb-6
-            (map (fn [{:story/keys [person message]}]
-                   [:div.p-2.rounded-xl.bg-green-400.mb-2
-                    [:h3.text-sm (:person/name person)]
-                    [:p.m-0 message]])
-                 prev-conv)])
-         (:conversation/previous-chats (first conversations)))]]]))
+      [:div.grow.bg-slate-700
+       [:div.flex.flex-row.bg-zinc-600
+        [:img.m-2.aspect-square.object-cover.w-12.h-12.rounded-full
+         {:src (or (get-in (first conversations) [:conversation/with :person/img])
+                   "/img/persons/test.jpg")}]
+        [:div.m-2
+         [:div (get-in (first conversations) [:conversation/with :person/name])]
+         [:div.text-sm (get-in (first conversations) [:conversation/with :person/organization])]]
+        ]
+       [:div.overflow-y-auto.chat-height
+        [:p.mt-2.p-2.text-center.text-sm.bg-slate-500 "Neue Nachrichten"]
+        (map
+          (fn [prev-conv]
+            [:div.m-3.max-w-2xl.mx-auto.px-3
+             (map (fn [{:story/keys [person message video]}]
+                    [:div.p-2.rounded-xl.bg-green-400.mb-2
+                     [:h3.text-sm (:person/name person)]
+                     [:p.m-0 message]])
+                  prev-conv)])
+          (:conversation/open-chats (first conversations)))
+
+        [:p.mt-2.p-2.text-center.text-sm.bg-slate-800 "Vorherige Chats"]
+        (components/conversation-widget (:conversation/previous-chats (first conversations)))]]]]))
